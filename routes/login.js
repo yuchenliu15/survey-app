589aef63742550feff7eecebe30bf85ea1c0f69c
yuchen <yl6448@nyu.edu>
2020-07-03 10:46:16 -0700

    login template

--- routes/login.js
+++ routes/login.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('authPage');
+  res.render('authPage', {type: "login"});
 });
 
 module.exports = router;
